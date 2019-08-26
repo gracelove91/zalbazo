c7bf90f5e30606f10ce33e26cb2f96e301b47d97
gracelove <govlmo91@gmail.com>
2019-08-26 19:27:08 +0900

    Controller를 위한 테스트 setUp 추가.

--- src/test/java/kr/zalbazo/controller/BasicControllerTest.java
+++ src/test/java/kr/zalbazo/controller/BasicControllerTest.java
@@ -1,7 +1,26 @@
 package kr.zalbazo.controller;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/root-context.xml")
 public class BasicControllerTest {
+
+    @Autowired
+    private BasicController basicController;
+
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setUp(){
+        this.mockMvc = MockMvcBuilders.standaloneSetup(basicController).build();
+    }
 
 }

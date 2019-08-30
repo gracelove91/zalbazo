fbff54335e4242f91bdbd7de6c7c26fd07fa2828
gracelove <govlmo91@gmail.com>
2019-08-30 16:25:45 +0900

    @PropertySource(application-dev.properties) => application-prod.properties

--- src/main/java/kr/zalbazo/config/DBConfig.java
+++ src/main/java/kr/zalbazo/config/DBConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.PropertySource;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:application-dev.properties")
+@PropertySource("classpath:application-prod.properties")
 public class DBConfig {
 
     @Value("${jdbc.driver-class}")

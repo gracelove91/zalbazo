18fb02417d8c130e7174eb151b7219226bcf83ea
gracelove <govlmo91@gmail.com>
2019-09-09 14:18:18 +0900

    asd

---
+++ src/main/java/kr/zalbazo/mapper/TimeMapper.java
@@ -0,0 +1,11 @@
+package kr.zalbazo.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {
+
+    @Select("SELECT sysdate from dual")
+    public String getTime2();
+
+    public String getTime();
+}

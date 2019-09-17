216be8c484e9beea16a9a519d53795f167ccf621
gette <upwyy123@gmail.com>
2019-09-17 19:48:10 +0900

    searchedlist 라벨추가 ,post -> get

--- src/main/java/kr/zalbazo/controller/search/SearchController.java
+++ src/main/java/kr/zalbazo/controller/search/SearchController.java
@@ -7,11 +7,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.zalbazo.model.hospital.Hospital;
+import kr.zalbazo.service.hospital.HospitalService;
 import kr.zalbazo.service.search.SearchService;
 import lombok.extern.log4j.Log4j;
 
@@ -21,33 +21,24 @@ public class SearchController {
 
 	@Autowired
 	private SearchService service;
+	@Autowired
+	private HospitalService hospitalService;
 
-//  @GetMapping("/in")
-//  public ModelAndView in2(@RequestParam(defaultValue="title") String searchOption,
-//	        @RequestParam(defaultValue="") String keyword)throws Exception{
-//	  List<Hospital> list = service.list();
-//	  ModelAndView mav = new ModelAndView();
-//	  Map<String, Object> map = new HashMap<String, Object>();
-//	  map.put("list", list); // list
-//	  mav.addObject("map", map); // 맵에 저장된 데이터를 mav에 저장
-//	  mav.setViewName("NewFile"); // 뷰를 list.jsp로 설정
-//	  return mav; // list.jsp로 List가 전달된다.
-//  }
 
-	@PostMapping("/hospital/searchedlist")
+	@GetMapping("/hospital/searchedlist")
 	public ModelAndView in(@RequestParam(defaultValue = "name") String searchOption,
 			@RequestParam(defaultValue = "") String keyword) throws Exception {
-		List<Hospital> list = service.listAll(searchOption, keyword);
-		System.out.println(searchOption);
-		System.out.println(keyword);
-// ModelAndView - 모델과 뷰
+		List<Hospital> list = service.listAll(searchOption, keyword); 
+		
+		//병원별 라벨 값 세팅
+		for (int i = 0; i < list.size(); i++) {
+			Hospital hospital = list.get(i);
+			hospital.setLabel(hospitalService.getLabelList(hospital.getHospitalId()));
+		}
+
+	
 		ModelAndView mav = new ModelAndView();
-		/*
-		 * mav.addObject("list", list); // 데이터를 저장 mav.addObject("count", count);
-		 * mav.addObject("searchOption", searchOption); mav.addObject("keyword",
-		 * keyword);
-		 */
-// 데이터를 맵에 저장
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("list", list); // list
 		map.put("searchOption", searchOption); // 검색옵션
@@ -58,3 +49,4 @@ public class SearchController {
 	}
 
 }
+

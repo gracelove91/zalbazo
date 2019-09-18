2e4734e332016bec2c92726123fdc7af92468845
gracelove <govlmo91@gmail.com>
2019-09-18 14:34:51 +0900

    유저join 유효성검사, 패스워드암호화, 메일인증, 주소찾기 구현

--- src/main/java/kr/zalbazo/model/user/User.java
+++ src/main/java/kr/zalbazo/model/user/User.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class User {
 
     @Email(message = "이메일의 형식이 아닙니다.") @NotBlank(message = "공백이 존재합니다.")  @NotEmpty(message = "비어있습니다")
-    private String email;
+    private String userEmail;
 
     @Size(min = 8, message = "비밀번호는 최소 8글자입니다")
     private String password;

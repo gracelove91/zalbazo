7d09d24937d158e50ebe1b4157e8b5647b0ae799
gracelove <govlmo91@gmail.com>
2019-09-20 13:12:11 +0900

    계층형 롤 설정, /resources/**, /webjars/** 로 들어오는 요청은 필터안타게 이그노잉.

--- src/main/java/kr/zalbazo/config/SecurityConfig.java
+++ src/main/java/kr/zalbazo/config/SecurityConfig.java
@@ -1,42 +1,53 @@
 package kr.zalbazo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
         http.authorizeRequests()
-//                .mvcMatchers("/resources/**").permitAll()
-//                .mvcMatchers("/webjars/**").permitAll()
-//                .mvcMatchers("/user/**").permitAll()
-//                .mvcMatchers("/", "/index").permitAll()
-//                .mvcMatchers("/jisikdong/**", "/community/**", "/hospital/**").hasAnyRole("user","admin")
                 .mvcMatchers("/admin/**").hasRole("admin")
-                .anyRequest().permitAll();
+                .anyRequest().authenticated()
+                .expressionHandler(expressionHandler());
 
 
         http.formLogin();
         http.httpBasic();
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                    .withUser("test").password("{noop}1234").roles("user")
-//                .and()
-//                    .withUser("admin").password("{noop}!@#$").roles("admin");
-//    }
+    private SecurityExpressionHandler<FilterInvocation> expressionHandler() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_admin > ROLE_user");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        return handler;
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/resources/**");
+        web.ignoring().mvcMatchers("/webjars/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }

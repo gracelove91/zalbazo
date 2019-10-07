178d1d9c98e5021e816ed4eeaf98a181b16cbb9b
bandalgomn <54057618+bandalgomn@users.noreply.github.com>
2019-10-07 14:56:34 +0900

    faq finished

--- src/main/java/kr/zalbazo/controller/FaqController.java
+++ src/main/java/kr/zalbazo/controller/FaqController.java
@@ -1,18 +1,44 @@
 package kr.zalbazo.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.zalbazo.model.content.Criteria;
+import kr.zalbazo.model.content.FaqVO;
+import kr.zalbazo.service.content.FaqService;
+
 @Controller
-@RequestMapping("/FAQ")
+@RequestMapping("/faq")
 public class FaqController {
 
-	@GetMapping("/FAQ")
+	@Autowired
+	FaqService service;
+	
+	public static final long Faq_Id = 2L;
+	
+	@GetMapping("/faq")
 	public String list() {
 		
-		System.out.println("ok");
-		return "FAQ";
+		return "faq";
 	}
+	
+	
+	@GetMapping("faq/faqsearchedlist")
+	public String searchedlist(Criteria cri, Model model, Long faqId, FaqVO keyword) {
+	
+	faqId = Faq_Id;
+	
+	System.out.println(faqId);
+	
+	model.addAttribute("FaqList", service.getFaqList(faqId));
+	model.addAttribute("FaqList2", service.getFaqList2(keyword));
+	
+	
+		return "faqsearchedlist";
+	}
+	
+
 }
---
+++ src/test/java/kr/zalbazo/content/FaqMapperTests.java
@@ -0,0 +1,36 @@
+package kr.zalbazo.content;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import kr.zalbazo.model.content.FaqVO;
+import kr.zalbazo.service.content.FaqService;
+import lombok.extern.log4j.Log4j;
+
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/views/exception/root-context.xml")
+@Log4j
+public class FaqMapperTests {
+	
+	@Autowired
+	FaqService service;
+	
+	@Test
+	public void test() {
+		
+	FaqVO faqvo = new FaqVO();
+	
+	faqvo.setKeyword("파");
+	
+	List<FaqVO> list = service.getFaqList2(faqvo);// getFaqList2(faqvo);  
+	
+
+	}	  
+
+}

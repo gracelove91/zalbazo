99f10c2f0938d76e2ba3b3c37ee2407aaccfbad0
gracelove <govlmo91@gmail.com>
2019-10-14 11:21:34 +0900

    충돌 해결

--- src/main/java/kr/zalbazo/mapper/user/UserMapper.java
+++ src/main/java/kr/zalbazo/mapper/user/UserMapper.java
@@ -7,10 +7,6 @@ import kr.zalbazo.model.user.User;
 
 public interface UserMapper {
     void insert(User user);
-<<<<<<< HEAD
-    User getUser(String email);
-=======
->>>>>>> 6fcd3a42383c488effd3675864d9631a22adc872
     List<User> getList();
     int delete(String email);
     int update(User user);

76f693885fa309ee3df800d5c45742c3282bc9bc
ej <48871461+clignancourt@users.noreply.github.com>
2019-10-16 16:07:26 +0900

    HospitalJoin service 수정

--- src/main/java/kr/zalbazo/service/user/HospitalJoinService.java
+++ src/main/java/kr/zalbazo/service/user/HospitalJoinService.java
@@ -1,6 +1,10 @@
 package kr.zalbazo.service.user;
 
+import java.util.List;
+
 import kr.zalbazo.model.hospital.Hospital;
+import kr.zalbazo.model.hospital.Label;
+import kr.zalbazo.model.pic.AttachFileDTO;
 
 public interface HospitalJoinService {
 	
@@ -9,5 +13,18 @@ public interface HospitalJoinService {
 	Hospital get(String userEmail);
 	
 	void modify(Hospital hospital);
+	
+    void infoInsert(Hospital hospital);
+    void labelInsert(Label label);
+    void picInsert(AttachFileDTO attachFile);
+    
+    int update(Hospital hospital);
+    
+    List<Label> getLabel(String userEmail);
+    List<AttachFileDTO> getPic(String userEmail);
+    
+    void removePicFirst(String userEmail);
+    void removePicLast();
+    void removeLabel(String userEmail);
 
 }

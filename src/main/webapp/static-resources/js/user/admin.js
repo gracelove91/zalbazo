c2e743ba297ecea662df74dbb0f99abcc31fcba3
ej <48871461+clignancourt@users.noreply.github.com>
2019-10-17 12:19:30 +0900

    운영자페이지 showList() 수정

--- src/main/webapp/static-resources/js/user/admin.js
+++ src/main/webapp/static-resources/js/user/admin.js
@@ -7,25 +7,30 @@ $(document).ready(function() {
 	var str1 = "";
 	var str2 = "";
 	
-	adminService.getUserList(function(list) {
-		
-		for(var i = 0, len = list.length||0; i < len; i++) {
-			//console.log(list[i]);
-	      
-	        str1 += "<tr>";
-			str1 += "<th scope='row' class='mobile' style='text-align:center;'>" + list[i].name + "</th>";
+	showUserList();
+	showHosList();
+	
+	function showUserList(){
+		adminService.getUserList(function(list) {
 			
-			str1 += "<td class='move' style='text-align:center;'>" + list[i].userEmail + "</td>";
-			str1 += "<td class='move' style='text-align:center;'>" + list[i].tel + "</td>";
-			str1 += "<td class='move' style='text-align:center;'>" + adminService.displayTime(list[i].joinDate) + "</td>";
-			str1 += "<td style='text-align:center;'><button class='btn btn-outline-secondary btn-sm removeUser' data-id='"+ list[i].userEmail+"'>X</button></td>";
+			for(var i = 0, len = list.length||0; i < len; i++) {
+				//console.log(list[i]);
+				
+				str1 += "<tr>";
+				str1 += "<th scope='row' class='mobile' style='text-align:center;'>" + list[i].name + "</th>";
+				
+				str1 += "<td class='move' style='text-align:center;'>" + list[i].userEmail + "</td>";
+				str1 += "<td class='move' style='text-align:center;'>" + list[i].tel + "</td>";
+				str1 += "<td class='move' style='text-align:center;'>" + adminService.displayTime(list[i].joinDate) + "</td>";
+				str1 += "<td style='text-align:center;'><button class='btn btn-outline-secondary btn-sm removeUser' data-id='"+ list[i].userEmail+"'>X</button></td>";
+				
+				str1 += "</tr>";
+			} // for
 			
-			str1 += "</tr>";
-		} // for
-		
-		userList.html(str1);
-		str1 = "";
-	}); // getUserList
+			userList.html(str1);
+			str1 = "";
+		}); // getUserList
+	}
 	
 	userList.on("click", ".removeUser", function(list, e) {
 		
@@ -35,7 +40,7 @@ $(document).ready(function() {
 		if(yes) {
 			adminService.getUser(userEmail, function(result){
 				alert("유저 삭제가 완료되었습니다");
-	            location.reload();
+				showUserList();
 			}, function(error){
 				console.log('에러에러');
 			});
@@ -44,24 +49,28 @@ $(document).ready(function() {
 		
 	}); // 유저 삭제 버튼 클릭
 	
-	adminService.getHosList(function(list) {
-		
-		for(var i = 0, len = list.length||0; i < len; i++) {
-			//console.log(list[i]);
+	
+	function showHosList(){
+		adminService.getHosList(function(list) {
 			
-			str2 += "<tr>";
+			for(var i = 0, len = list.length||0; i < len; i++) {
+				//console.log(list[i]);
+				
+				str2 += "<tr>";
+				
+				str2 += "<th scope='row' class='mobile' style='text-align:center;'><a href='/hospital/get?hospitalId=" + list[i].hospitalId + "'>" + list[i].name + "</a></th>";
+				str2 += "<td class='move' style='text-align:center;'>" + list[i].userEmail + "</td>";
+				str2 += "<td class='move' style='text-align:center;'>" + list[i].tel + "</td>";
+				str2 += "<td class='move' style='width:200px; text-align:center;'>" + list[i].address + "</td>";
+				str2 += "<td style='text-align:center;width:60px;'><button class='btn btn-outline-secondary btn-sm removeHos' data-id='"+ list[i].userEmail+"'>X</button></td>";
+				
+				str2 += "</tr>";
+			} // for
 			
-			str2 += "<th scope='row' class='mobile' style='text-align:center;'><a href='/hospital/get?hospitalId=" + list[i].hospitalId + "'>" + list[i].name + "</a></th>";
-			str2 += "<td class='move' style='text-align:center;'>" + list[i].userEmail + "</td>";
-			str2 += "<td class='move' style='text-align:center;'>" + list[i].tel + "</td>";
-			str2 += "<td class='move' style='width:200px; text-align:center;'>" + list[i].address + "</td>";
-			str2 += "<td style='text-align:center;width:60px;'><button class='btn btn-outline-secondary btn-sm removeHos' data-id='"+ list[i].userEmail+"'>X</button></td>";
-			
-			str2 += "</tr>";
-		} // for
-		
-		hospitalList.html(str2);
-	}); // getHosList
+			hospitalList.html(str2);
+			str2 = "";
+		}); // getHosList
+	}
 	
 	hospitalList.on("click", ".removeHos", function(list, e) {
 		
@@ -72,7 +81,8 @@ $(document).ready(function() {
 			
 			adminService.getHos(hoshos, function(result){
 				alert("병원 삭제가 완료되었습니다");
-				location.reload();
+				//location.reload();
+				showHosList();
 			}, function(error){
 				console.log('에러에러');
 			});
